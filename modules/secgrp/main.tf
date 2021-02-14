54178143ae0d15f677bb99591a3acdfde275d08f
anita <arav12378@gmail.com>
2021-02-14 11:45:16 -0600

    update backend

--- modules/secgrp/main.tf
+++ modules/secgrp/main.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "sg1" {
+resource "aws_security_group" "sg8" {
   #vpc_id = var.vpc
-  name = "sg1"
+  name = "sg8"
   ingress {
     description = "ssh"
     from_port   = 22
