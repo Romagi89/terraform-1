3989623196c9d76510dfba54eb893f99e8ed11a7
anita <arav12378@gmail.com>
2021-02-13 18:37:26 -0600

    update to jenkins

---
+++ test/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+      backend "s3" {
+      bucket = "anita-terraform"
+      key    = "test/terraform.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "terraformfile"
+  }
+}
