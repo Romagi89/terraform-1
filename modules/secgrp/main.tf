01893650c9f2cf516f7c376e3ceaedf8a474d945
anita <arav12378@gmail.com>
2021-02-14 10:35:07 -0600

    update pipeline

--- modules/secgrp/main.tf
+++ modules/secgrp/main.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "sg0" {
+resource "aws_security_group" "sg1" {
   #vpc_id = var.vpc
-  name = "sg0"
+  name = "sg1"
   ingress {
     description = "ssh"
     from_port   = 22
