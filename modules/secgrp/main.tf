ec6cfcef2af6575644f8a3b94d7f7fe1d8caa45e
anita <arav12378@gmail.com>
2021-02-14 12:57:11 -0600

    update test1

--- modules/secgrp/main.tf
+++ modules/secgrp/main.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "secgrp2" {
+resource "aws_security_group" "sg5" {
   #vpc_id = var.vpc
-  name = "secgrp2"
+  name = "sg5"
   ingress {
     description = "ssh"
     from_port   = 22
--- modules/secgrp/output.tf
+++ modules/secgrp/output.tf
@@ -1,3 +1,3 @@
 output "securityid" {
-    value = aws_security_group.secgrp2.id
+    value = aws_security_group.sg5.id
 }
